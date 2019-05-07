c25a436732943c621f5ba4a5d5456414ef9d02b2
ChaseAColvin <35513146+ChaseAColvin@users.noreply.github.com>
2019-05-07 16:41:01 -0600

    Initial upload of the PowerGrep module.
    Initial upload of the PowerGrep module.

---
+++ PowerGrep/PowerGrep.psm1
@@ -0,0 +1,39 @@
+﻿function grep
+{
+    [cmdletbinding()]
+
+    Param (
+    [string]$pattern=".*",
+    [switch]$e,
+    [switch]$i,
+    [switch]$v,
+    [switch]$c,
+    [parameter(ValueFromPipeline)]
+    [psobject[]]$input
+    )
+
+    $output = [System.Collections.ArrayList]@()
+
+    if ($input.Count -eq 0)
+    {
+        $input = Get-ChildItem -Path @(Get-Location)
+    }
+    
+    $HashArguments = @{
+        Pattern = $pattern
+        SimpleMatch = !$e
+        CaseSensitive = !$i
+        NotMatch = $v
+    }
+
+    $output += ($input | Out-String -Stream | Select-String @HashArguments)
+    
+    if ($c)
+    {
+        $output.Count
+    }
+    else
+    {
+        $output
+    }
+}
