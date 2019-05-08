b18734a6ba3333538536f103fb23620242cc0e49
ChaseAColvin <35513146+ChaseAColvin@users.noreply.github.com>
2019-05-08 15:51:05 -0600

    Extended PowerGrep Module
    Added more grep switches to the module. Also renamed the function in the module to conform to PowerShell naming standards, now Search-ObjectAsString, but added an alias for 'grep'.

--- PowerGrep/PowerGrep.psm1
+++ PowerGrep/PowerGrep.psm1
@@ -1,23 +1,24 @@
-﻿function grep
+﻿function Search-ObjectAsString
 {
     [cmdletbinding()]
-
-    Param (
-    [string]$pattern=".*",
+    param(
+    [Parameter(Position=0,
+    Mandatory=$true)]
+    [string]$pattern,
+    [Parameter(
+    Position=1,
+    ValueFromRemainingArguments)]
+    [array]$in,
+    [Parameter(ValueFromPipeline)]
+    $input,
     [switch]$e,
     [switch]$i,
     [switch]$v,
     [switch]$c,
-    [parameter(ValueFromPipeline)]
-    [psobject[]]$input
+    [switch]$n
     )
 
-    $output = [System.Collections.ArrayList]@()
-
-    if ($input.Count -eq 0)
-    {
-        $input = Get-ChildItem -Path @(Get-Location)
-    }
+    $out = [System.Collections.ArrayList]@()
     
     $HashArguments = @{
         Pattern = $pattern
@@ -26,14 +27,91 @@
         NotMatch = $v
     }
 
-    $output += ($input | Out-String -Stream | Select-String @HashArguments)
-    
-    if ($c)
+    if ($in)
     {
-        $output.Count
+        foreach ($item in $in)
+        {
+            $files = Get-ChildItem $item
+
+            foreach ($file in $files)
+            {
+                $content = Get-Content $file
+
+                $contentMatches = @($content | Out-String -Stream | Select-String @HashArguments)
+                
+                if ($c)
+                {
+                    if ($files.Count -gt 1)
+                    {
+                        $out += "$($file.Name):$($contentMatches.Count)"
+                    }
+                    else
+                    {
+                        $out += "$($contentMatches.Count)"
+                    }
+                }
+                else
+                {
+                    foreach ($contentMatch in $contentMatches)
+                    {
+                        if ($files.Count -gt 1)
+                        {
+                            if ($n)
+                            {
+                                $out += "$($file.Name):$($contentMatch.LineNumber):$($contentMatch.Line)"
+                            }
+                            else
+                            {
+                                $out += "$($file.Name):$($contentMatch.Line)"
+                            }
+                        }
+                        else
+                        {
+                            if ($n)
+                            {
+                                $out += "$($contentMatch.LineNumber):$($contentMatch.Line)"
+                            }
+                            else
+                            {
+                                $out += "$($contentMatch.Line)"
+                            }
+                        }
+                    }
+                }
+            }
+
+            $out
+        }
+    }
+    elseif ($input)
+    {
+        $contentMatches = @($input | Out-String -Stream | Select-String @HashArguments)
+
+        if ($c)
+        {
+            $contentMatches.Count
+        }
+        else
+        {
+            foreach ($contentMatch in $contentMatches)
+            {
+                if ($n)
+                {
+                    $out += "$($contentMatch.LineNumber):$($contentMatch.Line)"
+                }
+                else
+                {
+                    $out += "$($contentMatch.Line)"
+                }
+            }
+        }   
+
     }
     else
     {
-        $output
+        Throw "No input argument supplied."
+        Return 1
     }
 }
+
+Set-Alias -Name grep -Value Search-ObjectAsString
