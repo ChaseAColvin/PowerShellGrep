21d09c8308fd43b7cc909bf2158e3972d571e9fc
ccolvin <ccolvin@ebay.com>
2019-05-14 15:58:42 -0600

    Renaming the 'PowerGrep' module.
    Renaming the 'PowerGrep' module to 'PowerShellGrep', as there is currently an existing product out there called 'PowerGrep', which is a fat application used for advanced, regex based find and replace. This serves more as an interface/alias for the native PowerShell Select-String cmdlet, that allows it to be worked with like one would work with grep.

---
+++ PowerShellGrep/PowerShellGrep.psm1
@@ -0,0 +1,137 @@
+﻿function Search-ObjectAsString
+{
+    [cmdletbinding()]
+    param(
+    [Parameter(Position=0,
+    Mandatory=$true)]
+    [string]$pattern,
+    [Parameter(
+    Position=1,
+    ValueFromRemainingArguments)]
+    [array]$in,
+    [Parameter(ValueFromPipeline)]
+    $input,
+    [switch]$e,
+    [switch]$i,
+    [switch]$v,
+    [switch]$c,
+    [switch]$n
+    )
+
+    $out = [System.Collections.ArrayList]@()
+    $error = [System.Collections.ArrayList]@()
+
+    $HashArguments = @{
+        Pattern = $pattern
+        SimpleMatch = !$e
+        CaseSensitive = !$i
+        NotMatch = $v
+    }
+
+    if ($in)
+    {
+        $files = [System.Collections.ArrayList]@()
+
+        foreach ($item in $in)
+        {
+            if (Test-Path $item)
+            {
+                if ((Get-Item $item).Mode -notlike "d*")
+                {
+                    $files += Get-ChildItem $item | Where-Object {$_.Mode -notlike "d*"}
+                }
+                else
+                {
+                    $error += "grep: $($item): Is a directory"
+                }
+            }
+            else
+            {
+                $error += "grep: $($item): No such file or directory"
+            }
+        }
+
+        foreach ($file in ($files | Sort-Object -Unique))
+        {
+            $content = Get-Content $file.FullName
+
+            $contentMatches = @($content | Out-String -Stream | Select-String @HashArguments)
+                
+            if ($c)
+            {
+                if ($in.Count -gt 1)
+                {
+                    $out += "$($file.Name):$($contentMatches.Count)"
+                }
+                else
+                {
+                    $out += "$($contentMatches.Count)"
+                }
+            }
+            else
+            {
+                foreach ($contentMatch in $contentMatches)
+                {
+                    if ($in.Count -gt 1)
+                    {
+                        if ($n)
+                        {
+                            $out += "$($file.Name):$($contentMatch.LineNumber):$($contentMatch.Line)"
+                        }
+                        else
+                        {
+                            $out += "$($file.Name):$($contentMatch.Line)"
+                        }
+                    }
+                    else
+                    {
+                        if ($n)
+                        {
+                            $out += "$($contentMatch.LineNumber):$($contentMatch.Line)"
+                        }
+                        else
+                        {
+                            $out += "$($contentMatch.Line)"
+                        }
+                    }
+                }
+            }
+        }
+
+        $out
+        $error
+    }
+    elseif ($input)
+    {
+        $contentMatches = @($input | Out-String -Stream | Select-String @HashArguments)
+
+        if ($c)
+        {
+            $contentMatches.Count
+        }
+        else
+        {
+            foreach ($contentMatch in $contentMatches)
+            {
+                if ($n)
+                {
+                    $out += "$($contentMatch.LineNumber):$($contentMatch.Line)"
+                }
+                else
+                {
+                    $out += "$($contentMatch.Line)"
+                }
+            }
+            
+            $out
+        }   
+
+    }
+    else
+    {
+        Throw "No input argument supplied."
+        Return 1
+    }
+}
+
+Set-Alias -Name grep -Value Search-ObjectAsString
